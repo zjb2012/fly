6c41650e2a047df312e429b2b9166d609211f069
Jia Bao <zhangjiabao306@pingan.com.cn>
2019-01-25 00:50:57 -0800

    修改配置文件

---
+++ dtbs-web/src/main/java/Application.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by zhangjiabao on 29/05/2018.
+ */
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
